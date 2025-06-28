0d4e5f888f6ae0b03239a21b93955ea94f2c367a
rushxdev <rashmikapabasara@gmail.com>
2025-06-28 09:43:08 +0530

    dto created

--- backend/src/main/java/com/smart_idea/backend/dao/Idea.java
+++ backend/src/main/java/com/smart_idea/backend/dao/Idea.java
@@ -1,4 +1,30 @@
 package com.smart_idea.backend.dao;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name="ideas")
 public class Idea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    @Column(name="created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
