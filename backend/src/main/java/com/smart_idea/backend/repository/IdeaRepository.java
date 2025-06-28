9d9dd4939fe454b4b88571f8e2c1ab83c1974e34
rushxdev <rashmikapabasara@gmail.com>
2025-06-28 09:47:27 +0530

    repo created

--- backend/src/main/java/com/smart_idea/backend/repository/IdeaRepository.java
+++ backend/src/main/java/com/smart_idea/backend/repository/IdeaRepository.java
@@ -1,4 +1,10 @@
 package com.smart_idea.backend.repository;
 
-public class IdeaRepository {
+import com.smart_idea.backend.dao.Idea;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IdeaRepository extends JpaRepository<Idea, Long> {
+
 }
